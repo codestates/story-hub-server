abff330ebc37d2ac472c89ee82b59204c5f391c1
SungSeokMin <jkl154527@gmail.com>
2021-02-22 16:33:43 +0900

    feat/4/initial_setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,5 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: ['airbnb-base', 'plugin:@typescript-eslint/eslint-recommended'],
-  rules: { 'no-console': ['off'] },
+  rules: { 'no-console': ['off'], 'comma-dangle': ['off'], 'no-underscore-dangle': ['off'] },
 };
