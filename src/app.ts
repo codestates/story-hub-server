eddf56a1ed423e28bc24d8df2b2be81fcc75ce2b
Seongseokwon <pam9411@naver.com>
2021-02-26 11:33:13 +0900

    ♻️Refactor with TypeScript

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import express, { Application, urlencoded } from 'express';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+export default class App {
+  app : Application;
+
+  constructor(private port: number | string) {
+    this.port = port;
+    this.app = express();
+    this.settings();
+    this.middlewares();
+  }
+
+  settings() {
+    this.app.set('port', this.port || process.env.PORT || 3000);
+  }
+
+  middlewares() {
+    this.app.use(morgan('dev'));
+    this.app.use(express.json());
+    this.app.use(urlencoded({ extended: false }));
+    this.app.use(cookieParser());
+    this.app.use(cors({
+      origin: 'http://localhost:3000',
+      methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+      credentials: true
+    }));
+  }
+
+  async listen() {
+    await this.app.listen(this.app.get('port'));
+    console.log('server start on', this.app.get('port'));
+  }
+}
