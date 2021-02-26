c1e193c1cd278d2e1d5f9ca6d7b2032b0ddcf4a5
Seongseokwon <pam9411@naver.com>
2021-02-26 09:29:56 +0900

    🎨 lint modified

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,11 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: ['airbnb-base', 'plugin:@typescript-eslint/eslint-recommended'],
-  rules: { 'no-console': ['off'], 'comma-dangle': ['off'], 'no-underscore-dangle': ['off'] },
+  rules: {
+    'no-console': ['off'],
+    'comma-dangle': ['off'],
+    'no-underscore-dangle': ['off'],
+    'import/no-unresolved': ['off'],
+    'import/extensions': ['off'],
+  },
 };
