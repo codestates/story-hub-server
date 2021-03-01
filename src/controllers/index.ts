102c119fd3756e5a6850a523d7c069e520ee5988
Seongseokwon <pam9411@naver.com>
2021-03-01 19:11:20 +0900

    ✨ feat/58/comment_controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,9 +1,11 @@
 import oauthModule from './oauthController';
 import userModule from './userController';
+import commentModule from './commentController';
 
 const controllerModule = {
   oauthModule,
   userModule,
+  commentModule,
 };
 
 export default controllerModule;
--- src/routes/commentRouter.ts
+++ src/routes/commentRouter.ts
@@ -1,16 +1,18 @@
 import { Router } from 'express';
+import controllerModule from '../controllers';
 
+const { commentModule } = controllerModule;
 const commentRouter = Router();
 
 commentRouter
-  .get('/list')
-  .get('/alert_list')
-  .get('/info')
-  .post('/create')
-  .put('/')
-  .put('/like')
-  .put('/dislike')
-  .put('/alert')
-  .delete('/');
+  .get('/list', commentModule.list)
+  .get('/alert_list', commentModule.alert_list)
+  .get('/info', commentModule.info)
+  .post('/create', commentModule.create)
+  .put('/', commentModule.modify)
+  .put('/like', commentModule.like)
+  .put('/dislike', commentModule.dislike)
+  .put('/alert', commentModule.alert)
+  .delete('/', commentModule.delete);
 
 export default commentRouter;
