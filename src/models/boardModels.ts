d9541d5b3c6d84a226c684231538b740223cd000
SungSeokMin <jkl154527@gmail.com>
2021-03-03 13:06:44 +0900

    ♻️ refactoring_board_like

--- src/models/boardModels.ts
+++ src/models/boardModels.ts
@@ -79,18 +79,57 @@ const boardModels = {
     try {
       const conn = await connect();
 
-      // 해당 게시글 up_count + 1
+      const checkLikeSql = `
+      SELECT check_up_down FROM board_up_down WHERE email = ?
+      `;
+
+      const checkLike = await conn.query(checkLikeSql, [args.email]);
+      const checkLikeStr = JSON.parse(JSON.stringify(checkLike[0]));
+
+      const changeSql = `
+        UPDATE board_up_down SET check_up_down = 1 WHERE email = ?
+        `;
+
       const upCount = `
-      UPDATE boards SET up_count = up_count + 1 WHERE board_index = ?;
-      `;
-      await conn.query(upCount, [args.boardIndex]);
-      // board_up_down 테이블 email, board_index, check_up_down 1로 변경
-      const likeUser = `
-      INSERT INTO board_up_down (email, board_index, check_up_down) VALUES (?, ?, ?);
-      `;
-      await conn.query(likeUser, [args.email, args.boardIndex, true]);
+        UPDATE boards SET up_count = up_count + 1 WHERE email = ? AND board_index = ?;
+        `;
+
+      if (checkLikeStr.length === 0) {
+        await conn.query(upCount, [args.email, args.boardIndex]);
+
+        const insertSql = `
+        INSERT INTO board_up_down (email, board_index, check_up_down) VALUES (?, ?, ?);
+        `;
+
+        await conn.query(insertSql, [args.email, args.boardIndex, true]);
+      } else if (checkLikeStr[0].check_up_down === 0) {
+        // 좋아요를 누르려고 하는데 싫어요가 눌려져있는 상황
+        const upLike = `
+          UPDATE boards SET down_count = down_count - 1 WHERE email = ? AND board_index = ?;
+        `;
+
+        await conn.query(upLike, [args.email, args.boardIndex]);
+
+        await conn.query(changeSql, [args.email]);
+
+        await conn.query(upCount, [args.email, args.boardIndex]);
+      } else if (checkLikeStr[0].check_up_down === 1) {
+        // 좋아요를 누르려고 했는데 싫어요가 눌러져 있는 상황
+        const downSql = `
+          UPDATE boards SET up_count = up_count - 1 WHERE email = ? AND board_index = ?;
+        `;
+
+        await conn.query(downSql, [args.email, args.boardIndex]);
+
+        const removeBoardUpDown = `
+          DELETE FROM board_up_down WHERE email = ?;
+        `;
+
+        await conn.query(removeBoardUpDown, [args.email]);
+      }
       return 'OK';
     } catch (err) {
+      console.log(err);
       return err;
     }
   },
