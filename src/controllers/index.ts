72c99c0472d261ce5bfba6f08dc2a43f7b00aac3
SungSeokMin <jkl154527@gmail.com>
2021-03-04 13:41:56 +0900

    ✨ feat/60/commit_controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,12 +2,14 @@ import oauthModule from './oauthController';
 import userModule from './userController';
 import commentModule from './commentController';
 import boardModule from './boardController';
+import commitModule from './commitController';
 
 const controllerModule = {
   oauthModule,
   userModule,
   commentModule,
   boardModule,
+  commitModule,
 };
 
 export default controllerModule;
